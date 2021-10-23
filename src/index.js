9d63ca4a343b4780f5416ce1ca614bd1c431fc0d
Rafael Bulsing <rafabulsing@gmail.com>
2021-10-23 19:41:27 -0300

    remove file

--- src/index.js
+++
@@ -1,9 +0,0 @@
-const fs = require("fs");
-
-const json = require("./output.json");
-
-const sorted = json.sort((a, b) => {
-    return b.combo - a.combo;
-});
-
-fs.writeFileSync("./sorted.json", JSON.stringify(sorted, null, 4));
